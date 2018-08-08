306590be3593fc6a76e3cb8ffc4f12776f2ab182
Erica Bee <ebee511@gmail.com>
2018-08-08 08:29:52 -0700

    First commit

---
+++ sw.js
@@ -0,0 +1,66 @@
+var cacheName = 'restaurantLanding-v9';
+var filesToCache = [
+	'/',
+	'index.html', 
+	'README.md', 
+	'restaurant.html',
+	'css/restaurant-media-queries.css',
+	'css/neighborhoods-media-queries.css',
+	'css/styles.css',
+	'data/restaurants.json', 
+	'img/1.jpg',
+	'img/2.jpg',
+	'img/3.jpg',
+	'img/4.jpg',
+	'img/5.jpg', 
+	'img/6.jpg', 
+	'img/7.jpg', 
+	'img/8.jpg', 
+	'img/9.jpg', 
+	'img/10.jpg', 
+	'js/dbhelper.js',
+	'js/main.js',
+	'js/restaurant_info.js'
+];
+
+//Listen for install event, set callback;
+//Install cache
+//Skip waiting
+self.addEventListener('install', function(event) {
+	console.log('[ServiceWorker] Install');
+	event.waitUntil(
+		//open cache and give cache name, helps to create versions
+		caches.open(cacheName).then(function(cache) {
+			console.log('[ServiceWorker] Caching app shell');
+			//takes list of URLs, fetches them from server, adds response to the cache
+			return cache.addAll(filesToCache);
+		})
+	);
+	self.skipWaiting();
+});
+
+//Listen for activate event, delete old cache and install new cache
+self.addEventListener('activate', function(event) {
+	console.log('[ServiceWorker] Activate');
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key !== cacheName) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}))
+		})
+	);
+});
+
+//Fetch the data
+self.addEventListener('fetch', function(event) {
+	//tells the browser we will handle the response ourselves
+	console.log('[ServiceWorker] Fetch', event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
